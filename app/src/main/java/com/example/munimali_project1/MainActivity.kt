34deca5183e72f7d13b416f59f8c4fa4438016a6
BT_1N3_24 <example@bths.edu>
2024-10-09 09:33:32 -0400

    needa figure it out

--- app/src/main/java/com/example/munimali_project1/MainActivity.kt
+++ app/src/main/java/com/example/munimali_project1/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.munimali_project1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var scoreView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val layout: LinearLayout = findViewById(R.id.main)
+        resources.getString(R.string.score, "TEST")
+        scoreView = findViewById(R.id.score)
+        val strikeView: TextView = findViewById(R.id.strikes)
+        val startRestartButton: Button = findViewById(R.id.start_restart_button)
+        val directionText: TextView = findViewById(R.id.directions_text)
+        val topButton: Button = findViewById(R.id.top_button)
+        val bottomButton: Button = findViewById(R.id.bottom_button)
+
+    }
+
+    fun startGame() {
+        scoreView
     }
 }
