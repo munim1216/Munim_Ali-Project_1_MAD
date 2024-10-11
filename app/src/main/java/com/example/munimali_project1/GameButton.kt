3cc07ea3d75b63c2170b70639da94e09d0fb76d4
BT_1N3_24 <example@bths.edu>
2024-10-11 09:33:45 -0400

    trying to figureo ut custom views

---
+++ app/src/main/java/com/example/munimali_project1/GameButton.kt
@@ -0,0 +1,7 @@
+package com.example.munimali_project1
+
+import android.content.Context
+
+class GameButton(context: Context) : androidx.appcompat.widget.AppCompatButton(context) {
+    var active = false
+}
--- app/src/main/java/com/example/munimali_project1/MainActivity.kt
+++ app/src/main/java/com/example/munimali_project1/MainActivity.kt
@@ -12,19 +12,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var scoreView: TextView
+    private lateinit var strikeView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val layout: LinearLayout = findViewById(R.id.main)
-        resources.getString(R.string.score, "TEST")
         scoreView = findViewById(R.id.score)
-        val strikeView: TextView = findViewById(R.id.strikes)
+        strikeView = findViewById(R.id.strikes)
         val startRestartButton: Button = findViewById(R.id.start_restart_button)
         val directionText: TextView = findViewById(R.id.directions_text)
         val topButton: Button = findViewById(R.id.top_button)
         val bottomButton: Button = findViewById(R.id.bottom_button)
+
+        topButton.setOnClickListener {
+            scoreView.text = getString(R.string.score, 8888)
+        }
 
     }
 
